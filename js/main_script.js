4d70eadcf4796b1bd5251579dc44a3eb384032e2
ShinYoung-hwan <syh754200@g.skku.edu>
2024-09-22 13:05:35 +0900

    refactoring codes

--- js/main_script.js
+++ js/main_script.js
@@ -1,18 +1,10 @@
-const displayCircleDot = (srcPos, dstPos, distance, map) => {
-  // 클릭 지점을 표시할 빨간 동그라미 커스텀오버레이를 생성합니다
+/* 출발지점과 도착지점 가운데에 총 거리를 표시합니다. */
+const show_total_distance = (srcPos, dstPos, distance, map) => {
+  // 가운데 지점의 좌표 구하기
   const position = new kakao.maps.LatLng(
     (srcPos.Ma + dstPos.Ma) / 2,
     (srcPos.La + dstPos.La) / 2
   );
-
-  var circleOverlay = new kakao.maps.CustomOverlay({
-    content: '<span class="dot"></span>',
-    position: position,
-    zIndex: 1,
-  });
-
-  // 지도에 표시합니다
-  circleOverlay.setMap(map);
 
   if (distance > 0) {
     // 클릭한 지점까지의 그려진 선의 총 거리를 표시할 커스텀 오버레이를 생성합니다
@@ -31,23 +23,27 @@ const displayCircleDot = (srcPos, dstPos, distance, map) => {
   }
 };
 
+/* 주어진 imageSrc를 기반으로 지도에 표시할 마커를 생성한다. */
 const get_marker_image = (imageSrc) => {
-  var imageSize = new kakao.maps.Size(42, 46), // 마커이미지의 크기입니다
+  var imageSize = new kakao.maps.Size(42, 46), // 마커이미지의 크기
     imageOption = { offset: new kakao.maps.Point(18, 46) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
 
   // 마커의 이미지정보를 가지고 있는 마커이미지를 생성해 반환
   return new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
 };
 
-//
-const set_current_position = () => {
+/* 주어진 위치들을 지도상에 표시한다. */
+const set_positions = (lat = null, lng = null) => {
   // 현재위치 찍기
   navigator.geolocation.getCurrentPosition((position) => {
+    // 맵 바운더리 설정을 위한 변수
+    var bounds = new kakao.maps.LatLngBounds();
     // 현재 위치 객체
     const srcPos = new kakao.maps.LatLng(
       position.coords.latitude,
       position.coords.longitude
     );
+    bounds.extend(srcPos);
 
     // 지도 중앙 설정
     var map = new kakao.maps.Map(container, {
@@ -60,64 +56,47 @@ const set_current_position = () => {
       position: srcPos,
       image: get_marker_image("./assets/src_marker.png"), // 마커이미지 설정
     });
-
-    marker.setMap(map);
-  });
-};
-
-//
-const set_target_position = (lat, lng) => {
-  // 현재위치와 목표 위치를 동시에 찍기
-  navigator.geolocation.getCurrentPosition((position) => {
-    // 맵 바운더리 설정을 위한 변수
-    var bounds = new kakao.maps.LatLngBounds();
-
-    // 현재 위치 객체
-    const srcPos = new kakao.maps.LatLng(
-      position.coords.latitude,
-      position.coords.longitude
-    );
-    bounds.extend(srcPos);
-    var map = new kakao.maps.Map(container, {
-      center: srcPos,
-      level: 3,
-    });
-
-    // 마커를 생성합니다
-    var marker = new kakao.maps.Marker({
-      position: srcPos,
-      image: get_marker_image("./assets/src_marker.png"), // 마커이미지 설정
-    });
-
     marker.setMap(map);
 
-    // 목적지 위치 객체
-    const dstPos = new kakao.maps.LatLng(lat, lng);
-    bounds.extend(dstPos);
+    // 도착 지점이 정해져 있다면
+    if (lat !== null && lng !== null) {
+      // 목적지 위치 객체
+      const dstPos = new kakao.maps.LatLng(lat, lng);
+      bounds.extend(dstPos);
 
-    // 마커를 생성합니다
-    marker = new kakao.maps.Marker({
-      position: dstPos,
-      image: get_marker_image("./assets/dst_marker.png"), // 마커이미지 설정
-    });
+      // 마커를 생성합니다
+      marker = new kakao.maps.Marker({
+        position: dstPos,
+        image: get_marker_image("./assets/dst_marker.png"), // 마커이미지 설정
+      });
+      marker.setMap(map);
 
-    marker.setMap(map);
+      // 지도에 직선거리 표시하기
+      var polyline = new kakao.maps.Polyline({
+        path: [srcPos, dstPos], // 선을 구성하는 좌표배열 입니다
+        strokeWeight: 5, // 선의 두께 입니다
+        strokeColor: "#FFAE00", // 선의 색깔입니다
+        strokeOpacity: 0.7, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+        strokeStyle: "solid", // 선의 스타일입니다
+      });
+
+      // 지도에 선을 표시합니다
+      polyline.setMap(map);
+      var distance = Math.round(polyline.getLength());
+      show_total_distance(srcPos, dstPos, distance, map);
+    }
 
     // 맵 크기 재설정
     map.setBounds(bounds);
-
-    // 지도에 직선거리 표시하기
-    var polyline = new kakao.maps.Polyline({
-      path: [srcPos, dstPos], // 선을 구성하는 좌표배열 입니다
-      strokeWeight: 5, // 선의 두께 입니다
-      strokeColor: "#FFAE00", // 선의 색깔입니다
-      strokeOpacity: 0.7, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
-      strokeStyle: "solid", // 선의 스타일입니다
-    });
-
-    // 지도에 선을 표시합니다
-    polyline.setMap(map);
-    var distance = Math.round(polyline.getLength());
-    displayCircleDot(srcPos, dstPos, distance, map);
   });
+};
+
+/* 현재 위치를 지도상에 표시합니다. */
+const set_current_position = (lat = null, lng = null) => {
+  set_positions(lat, lng);
+};
+
+/* 현재위치로부터 목표 위치까지의 직선 경로를 표시합니다. */
+const set_target_position = (lat, lng) => {
+  set_positions(lat, lng);
 };
--- js/sidebar_script.js
+++ js/sidebar_script.js
@@ -1,4 +1,5 @@
-const show_building_info = (building_info) => {
+/* 클릭한 건물에 대한 자세한 정보를 보여준다. */
+const show_building_info_overlay = (building_info) => {
   modal_title.replaceChildren();
   modal_title.append(
     `${building_info.building_id}: ${building_info.building_name} - ${building_info.campus}`
@@ -14,22 +15,22 @@ const show_building_info = (building_info) => {
   building_image.setAttribute("style", "width: 100%;");
 
   const building_detail = document.createElement("div");
-  building_detail.append(" " + building_info.building_info);
+  building_detail.append(building_info.building_info);
 
   modal_body.append(building_image);
   modal_body.append(building_detail);
 };
 
-//
-const set_buildings_list = () => {
+/* 주어진 입력에 맞게 building list를 최신화해준다. */
+const set_buildings_list = (text) => {
   buildings_nav.replaceChildren(); // 비워주기
 
   for (building of building_infos) {
     if (
-      input.value &&
+      text &&
       !(
-        building.building_id.includes(input.value) ||
-        building.building_name.includes(input.value)
+        building.building_id.includes(text) ||
+        building.building_name.includes(text)
       )
     )
       continue;
@@ -54,7 +55,7 @@ const set_buildings_list = () => {
     span.setAttribute("data-bs-target", "#overlayModal");
     span.setAttribute(
       "onclick",
-      `show_building_info(${JSON.stringify(building)})`
+      `show_building_info_overlay(${JSON.stringify(building)})`
     );
     var i = document.createElement("i");
     i.setAttribute("class", "bi bi-info-circle");
